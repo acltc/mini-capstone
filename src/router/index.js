c7815846428d7cce143ba75a86b39541cf4e7954
Peter Jang <peter.x.jang@gmail.com>
2021-03-10 16:22:15 -0600

    Add model associations exercise

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ import Exercise1ProductsShow from "../views/exercise1/ProductsShow.vue";
 import Exercise1ProductsNew from "../views/exercise1/ProductsNew.vue";
 import Exercise1ProductsEdit from "../views/exercise1/ProductsEdit.vue";
 
+import Exercise3App from "../views/exercise3/App.vue";
+import Exercise3ProductsIndex from "../views/exercise3/ProductsIndex.vue";
+import Exercise3ProductsShow from "../views/exercise3/ProductsShow.vue";
+import Exercise3ProductsNew from "../views/exercise3/ProductsNew.vue";
+import Exercise3ProductsEdit from "../views/exercise3/ProductsEdit.vue";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -33,6 +39,32 @@ const routes = [
         path: "products/:id/edit",
         component: Exercise1ProductsEdit,
         name: "exercise1-products-edit",
+      },
+    ],
+  },
+  {
+    path: "/model-associations",
+    component: Exercise3App,
+    children: [
+      {
+        path: "products",
+        component: Exercise3ProductsIndex,
+        name: "exercise3-products-index",
+      },
+      {
+        path: "products/new",
+        component: Exercise3ProductsNew,
+        name: "exercise3-products-new",
+      },
+      {
+        path: "products/:id",
+        component: Exercise3ProductsShow,
+        name: "exercise3-products-show",
+      },
+      {
+        path: "products/:id/edit",
+        component: Exercise3ProductsEdit,
+        name: "exercise3-products-edit",
       },
     ],
   },
---
+++ src/views/exercise3/ProductsEdit.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="home">
+    <a
+      class="btn btn-outline-info m-3 float-right"
+      data-toggle="collapse"
+      href="#collapseDebugInfo"
+      role="button"
+      aria-expanded="false"
+      aria-controls="collapseDebugInfo"
+    >
+      &#9432;
+    </a>
+    <div class="collapse" id="collapseDebugInfo">
+      <div class="card card-body">
+        <p>
+          <strong>Data from</strong>
+          : GET {{ appConfig.domain }}{{ appConfig.productsUrl }}/{{ $route.params.id }}
+        </p>
+        <p>
+          <strong>Data shape</strong>
+          :
+          <span v-html="wordifiedSchema"></span>
+        </p>
+        <br />
+        <p>
+          <strong>Data sending to</strong>
+          : PATCH {{ appConfig.domain }}{{ appConfig.productsUrl }}/{{ $route.params.id }}
+        </p>
+        <p>
+          <strong>Using params</strong>
+          :
+          <strong>name</strong>
+          ,
+          <strong>price</strong>
+          ,
+          <strong>description</strong>
+          (can't edit images or supplier)
+        </p>
+      </div>
+    </div>
+    <div class="container">
+      <form v-on:submit.prevent="submit()">
+        <h1>Edit product</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+          <label>Name:</label>
+          <input type="text" class="form-control" v-model="product.name" />
+        </div>
+        <div class="form-group">
+          <label>Price:</label>
+          <input type="text" class="form-control" v-model="product.price" />
+        </div>
+        <div class="form-group">
+          <label>Description:</label>
+          <input type="text" class="form-control" v-model="product.description" />
+        </div>
+        <input type="submit" class="btn btn-primary mr-1" value="Submit" />
+        <router-link :to="{ name: 'exercise3-products-show', params: { id: product.id } }" class="btn btn-secondary">
+          Cancel
+        </router-link>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SchemaValidator from "../../mixins/SchemaValidator.js";
+
+export default {
+  mixins: [SchemaValidator],
+  props: ["appConfig"],
+  data: function() {
+    return {
+      product: { id: 0 },
+      schema: {
+        type: "object",
+        properties: {
+          productsIdKey: {
+            alias: "id",
+            type: "integer",
+          },
+          productsNameKey: {
+            alias: "name",
+            type: "string",
+          },
+          productsPriceKey: {
+            alias: "price",
+            type: "number",
+          },
+          productsDescriptionKey: {
+            alias: "description",
+            type: "string",
+          },
+        },
+      },
+      errors: [],
+    };
+  },
+  created: function() {
+    axios
+      .get(this.appConfig.domain + this.appConfig.productsUrl + "/" + this.$route.params.id)
+      .then(response => {
+        this.product = this.formatDataToSchema(response.data) || this.product;
+      })
+      .catch(error => {
+        this.$emit("showError", ["domain", "productsUrl"], error.request.status);
+      });
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.product.name,
+        price: this.product.price,
+        description: this.product.description,
+      };
+      axios
+        .patch(this.appConfig.domain + this.appConfig.productsUrl + "/" + this.$route.params.id, params)
+        .then(response => {
+          this.$router.push({ name: "exercise3-products-show", params: { id: response.data.id } });
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
